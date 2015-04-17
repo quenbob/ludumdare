f2dbeec5d48dbec3f683f78d5532adbce04e8446
Samuel Gélineau <sgelineau@innobec.com>
2015-04-17 19:47:08 -0400

    script to move the player's legs

---
+++ Assets/Scripts (samuel)/PlayerLegsAnimation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLegsAnimation : MonoBehaviour {
+
+	public float legWigglePerSecond = 2;
+	public float legExtentInDegrees = 45;
+
+	private float t = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t += Time.deltaTime;
+		float wiggles = legWigglePerSecond * t;
+		float angle = Mathf.Sin (wiggles * 2*Mathf.PI) * legExtentInDegrees;
+
+		GameObject leftLeg = GameObject.Find("Left leg");
+		GameObject rightLeg = GameObject.Find("Right leg");
+
+		leftLeg.transform.rotation = Quaternion.Euler(new Vector3 (angle, 0, 0));
+		rightLeg.transform.rotation = Quaternion.Euler(new Vector3 (-angle, 0, 0));
+	}
+}

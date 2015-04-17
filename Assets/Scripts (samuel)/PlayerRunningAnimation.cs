c54fb480ee97d2724349afcd0af242337f072a97
Samuel Gélineau <sgelineau@innobec.com>
2015-04-17 19:56:13 -0400

    bounce up and down as the player runs

--- Assets/Scripts (samuel)/PlayerRunningAnimation.cs
+++ Assets/Scripts (samuel)/PlayerRunningAnimation.cs
@@ -5,6 +5,7 @@ public class PlayerRunningAnimation : MonoBehaviour {
 
 	public float legWigglePerSecond = 2;
 	public float legExtentInDegrees = 45;
+	public float bounceHeight = 0.1f;
 
 	private float t = 0;
 
@@ -18,11 +19,13 @@ public class PlayerRunningAnimation : MonoBehaviour {
 		t += Time.deltaTime;
 		float wiggles = legWigglePerSecond * t;
 		float angle = Mathf.Sin (wiggles * 2*Mathf.PI) * legExtentInDegrees;
+		float height = Mathf.Abs (Mathf.Sin (wiggles * 2*Mathf.PI) * bounceHeight);
 
-		GameObject playerBody = GameObject.Find("Body");
+		GameObject playerModel = GameObject.Find("Model");
 		GameObject leftLeg = GameObject.Find("Left leg");
 		GameObject rightLeg = GameObject.Find("Right leg");
 
+		playerModel.transform.position = new Vector3(playerModel.transform.position.x, height, playerModel.transform.position.z);
 		leftLeg.transform.rotation = Quaternion.Euler(new Vector3 (angle, 0, 0));
 		rightLeg.transform.rotation = Quaternion.Euler(new Vector3 (-angle, 0, 0));
 	}

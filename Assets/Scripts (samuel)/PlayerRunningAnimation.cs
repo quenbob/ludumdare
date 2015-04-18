817fd882b2f77ab4e6f1e7b16404661a4340509d
Samuel Gélineau <sgelineau@innobec.com>
2015-04-17 20:06:45 -0400

    press a key to run

--- Assets/Scripts (samuel)/PlayerRunningAnimation.cs
+++ Assets/Scripts (samuel)/PlayerRunningAnimation.cs
@@ -7,16 +7,34 @@ public class PlayerRunningAnimation : MonoBehaviour {
 	public float legExtentInDegrees = 45;
 	public float bounceHeight = 0.1f;
 
+	private bool isRunning = false;
 	private float t = 0;
 
+	public void startRunning() {
+		if (!isRunning) {
+			t = 0;
+			isRunning = true;
+		}
+	}
+	
+	public void stopRunning() {
+		if (isRunning) {
+			t = 0;
+			isRunning = false;
+		}
+	}
+	
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		t += Time.deltaTime;
+		if (isRunning) {
+			t += Time.deltaTime;
+		}
+
 		float wiggles = legWigglePerSecond * t;
 		float angle = Mathf.Sin (wiggles * 2*Mathf.PI) * legExtentInDegrees;
 		float height = Mathf.Abs (Mathf.Sin (wiggles * 2*Mathf.PI) * bounceHeight);

581e3757defcc2601fbbfd228883d436d3477442
Samuel Gélineau <sgelineau@innobec.com>
2015-04-18 10:13:13 -0400

    demo: press a key to toggle the tree state

---
+++ Assets/sgelineau/AnyKeyToToggleTreeState.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnyKeyToToggleTreeState : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject tree = GameObject.Find ("Tree");
+		TreeState treeState = tree.GetComponent<TreeState> ();
+
+		if (Input.anyKeyDown) {
+			switch (treeState.currentState) {
+			case TreeStateEnum.Stump:
+				treeState.currentState = TreeStateEnum.Sprout;
+				break;
+			case TreeStateEnum.Sprout:
+				treeState.currentState = TreeStateEnum.Grown;
+				break;
+			case TreeStateEnum.Grown:
+				treeState.currentState = TreeStateEnum.Stump;
+				break;
+			}
+		}	
+	}
+}

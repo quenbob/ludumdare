d78caab94960595d07baddda9f20126731ee665d
romain41 <freeway_87@hotmail.com>
2015-04-18 17:48:01 -0400

    Code clean-up bis.

---
+++ Assets/romain/scripts/RiverAnimator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RiverAnimator : MonoBehaviour {
+	public float scrollSpeed = 0.20f;
+	public float offset = 0f;
+	
+	void Update () {
+		offset += (Time.deltaTime * scrollSpeed) / 10.0f;
+		GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+	}
+}

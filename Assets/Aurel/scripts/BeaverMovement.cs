c73927f7821ad651c3d5cc79e00d33322ed517f7
Aurelien Carrere <acarrere@innobec.com>
2015-04-18 18:41:37 -0400

    bug correction

--- Assets/Aurel/scripts/BeaverMovement.cs
+++ Assets/Aurel/scripts/BeaverMovement.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class BeaverMovement : MonoBehaviour {
 	public PlayerAttack playerAttack;
 
+	private float angle = 0.0f;
+	private bool direction = false;
+
 	// Use this for initialization
 	void Start () {
-		playerAttack = transform.Find("../Player").gameObject.GetComponent<PlayerAttack>()
+		playerAttack = transform.Find("../Player").gameObject.GetComponent<PlayerAttack>();
 	}
 	
 	// Update is called once per frame
@@ -15,5 +18,22 @@ public class BeaverMovement : MonoBehaviour {
 		{
 
 		}
+	}
+
+	void Move()
+	{
+		if (direction)
+		{
+			angle += 5.0f;
+		}
+		else
+		{
+			angle -= 5.0f;
+		}
+	}
+
+	void StopMove()
+	{
+
 	}
 }

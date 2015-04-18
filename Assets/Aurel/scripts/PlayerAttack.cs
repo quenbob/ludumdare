74ee02ace8504bbd51b695764af2a1e982c14b14
Aurelien Carrere <acarrere@innobec.com>
2015-04-18 14:19:58 -0400

    attack !!!!

--- Assets/Aurel/scripts/PlayerAttack.cs
+++ Assets/Aurel/scripts/PlayerAttack.cs
@@ -3,13 +3,50 @@ using System.Collections;
 
 public class PlayerAttack : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public float timeBetweenAttacks = 0.5f;
+	public int attackDamage = 10;
 	
+	bool treeInRange;
+	float timer;
+	TreeHealth health;
+	
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.tag == "Tree")
+		{
+			health = other.gameObject.GetComponent<TreeHealth>();
+			treeInRange = true;
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject.tag == "Tree")
+		{
+			treeInRange = false;
+		}
+	}
+	
+	
+	void Update ()
+	{
+		timer += Time.deltaTime;
+		
+		if(timer >= timeBetweenAttacks && treeInRange)
+		{
+			Attack ();
+		}
+	}
+	
+	
+	void Attack ()
+	{
+		timer = 0.0f;
+		
+		if(health.currentHealth > 0)
+		{
+			health.TakeDamage(attackDamage);
+		}
 	}
 }
---
+++ Assets/sgelineau/TreeHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeHealth : MonoBehaviour {
+
+	public float currentHealth = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TakeDamage(float amount)
+	{
+
+	}
+}

11cd707c9c4c46e3a8c1b5a5c414750a7b619f93
Samuel Gélineau <sgelineau@innobec.com>
2015-04-18 12:09:46 -0400

    trees fall when you touch them

---
+++ Assets/sgelineau/TreeFalling.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeFalling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.collider.gameObject.tag == "Player") {
+			Rigidbody rigidBody = GetComponent<Rigidbody> ();
+			rigidBody.isKinematic = false;
+			rigidBody.AddForce(new Vector3(1,0,0));
+
+			GetComponent<Collider>().isTrigger = true;
+		}
+	}
+}

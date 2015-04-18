9adcec2c9ae060ba3c896a483284ff3fcb275c78
Aurelien Carrere <acarrere@innobec.com>
2015-04-18 12:46:55 -0400

    Camera follow and zoom

---
+++ Assets/Aurel/scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5.0f;
+	public float minFov = 15f;
+	public float maxFov = 90f;
+	public float sensitivity = 10f;
+
+	private Vector3 offset;
+	
+	void Start ()
+	{
+		offset = Camera.main.transform.position - target.position;
+	}
+	
+	void FixedUpdate ()
+	{
+		Vector3 targetCamPos = target.position + offset;
+		Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+	
+	void Update () {
+		float fov  = Camera.main.fieldOfView;
+		fov -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+		fov = Mathf.Clamp(fov, minFov, maxFov);
+		Camera.main.fieldOfView = fov;
+	}
+}

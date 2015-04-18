f0469ad45e13068e583c5fbe53450f521931157d
Aurelien Carrere <acarrere@innobec.com>
2015-04-18 10:49:07 -0400

    Player prefab with movement script

---
+++ Assets/Aurel/scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 60.0f;
+	public float turnSmoothing = 15f;
+
+	private Vector3 movement;
+	private Rigidbody playerRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		playerRigidbody = GetComponent <Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+
+		if (h != 0 || v != 0)
+			Turn(h, v);	
+	}
+
+	void Move (float h, float v)
+	{
+		movement.Set (h, 0.0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+	
+	void Turn(float h, float v) {
+		Vector3 targetDirection = new Vector3(h, 0f, v);
+		Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+		Quaternion newRotation = Quaternion.Lerp(playerRigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+		playerRigidbody.MoveRotation(newRotation);
+	}
+}

9de3d7d247c625c835a14df1bbb84c8fb5c427e9
Aurelien Carrere <acarrere@innobec.com>
2015-04-18 18:36:33 -0400

    bigger beaver

--- Assets/Aurel/scripts/BeaverMovement.cs
+++ Assets/Aurel/scripts/BeaverMovement.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class BeaverMovement : MonoBehaviour {
-	public GameObject player;
+	public PlayerAttack playerAttack;
 
 	// Use this for initialization
 	void Start () {
+		playerAttack = transform.Find("../Player").gameObject.GetComponent<PlayerAttack>()
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(playerAttack.treeInRange)
+		{
+
+		}
 	}
 }

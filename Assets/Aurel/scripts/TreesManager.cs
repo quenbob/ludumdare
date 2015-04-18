a35367db8fe61c9e74497f3f681d80533658c537
Aurelien Carrere <acarrere@innobec.com>
2015-04-18 12:13:52 -0400

    spawning manager improvement

--- Assets/Aurel/scripts/TreesManager.cs
+++ Assets/Aurel/scripts/TreesManager.cs
@@ -6,11 +6,12 @@ public class TreesManager : MonoBehaviour {
 	public GameObject treeObject;
 	public float timeBeforeFirstSpawn = 3.0f;
 	public float timeBetweenTwoSpawn = 3.0f;
-	public float xMin = -10.0f;
-	public float xMax = 10.0f;
-	public float zMin = -10.0f;
-	public float zMax = 10.0f;
+	public float xMin = -100.0f;
+	public float xMax = 100.0f;
+	public float zMin = -100.0f;
+	public float zMax = 100.0f;
 	public bool canSpawn = true ;
+	public float clearRadiusAroundNewSpawn = 5.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -22,11 +23,24 @@ public class TreesManager : MonoBehaviour {
 		{
 			return;
 		}
-		
-		float x = Random.Range (xMin, xMax);
-		float z = Random.Range (zMin, zMax);
-		Quaternion rot = Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), Vector3.up);
 
-		Instantiate(treeObject, new Vector3(x, 0.0f, z), rot);
+		Vector3 pos = new Vector3();
+		int nbTry = 0;
+		bool ok = false;
+
+		while(!ok && nbTry < 10) {
+			nbTry++;
+			pos = new Vector3(Random.Range (xMin, xMax), 0.0f, Random.Range (zMin, zMax));
+			Collider[] cols = Physics.OverlapSphere(pos, clearRadiusAroundNewSpawn);
+
+			if (cols.Length == 0)
+				ok = true;
+		}
+
+		if (ok)
+		{
+			Quaternion rot = Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), Vector3.up);
+			Instantiate(treeObject, pos, rot);
+		}
 	}
 }

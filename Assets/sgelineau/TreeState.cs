61ed0f3f0994646c8cc33344e70b63754f1bda7f
Samuel Gélineau <sgelineau@innobec.com>
2015-04-18 10:00:35 -0400

    mock trees, can switch between sprout, grown and stumps

---
+++ Assets/sgelineau/TreeState.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TreeStateEnum {
+	Sprout, Grown, Stump
+}
+
+public class TreeState : MonoBehaviour {
+
+	public TreeStateEnum currentState = TreeStateEnum.Grown;
+
+	// Use this for initialization
+	void Start () {
+		GameObject sprout = GameObject.Find("Sprout");
+		GameObject grown = GameObject.Find("Grown");
+		GameObject stump = GameObject.Find("Stump");
+
+		sprout.SetActive(currentState == TreeStateEnum.Sprout);
+		grown.SetActive(currentState == TreeStateEnum.Grown);
+		stump.SetActive(currentState == TreeStateEnum.Stump);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

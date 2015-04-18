8918ae10328449278b1a35f5afabc2b4114c65a8
romain41 <freeway_87@hotmail.com>
2015-04-18 14:11:14 -0400

    Add a script to animate river.

---
+++ Assets/romain/RiverAnimator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RiverAnimator : MonoBehaviour {
+	float scrollSpeed = 0.35f;
+	float offset = 0f;
+	
+	void Update () {
+		offset += (Time.deltaTime * scrollSpeed) / 10.0f;
+		GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+	}
+}

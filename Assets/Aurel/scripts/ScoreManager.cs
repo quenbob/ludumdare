cb766b5f728cd43c18b79c59d78f8e4ce9586f64
Aurelien Carrere <acarrere@innobec.com>
2015-04-18 16:59:28 -0400

    ui

---
+++ Assets/Aurel/scripts/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+	
+	public Text scoreLabel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void AddScore(int amount)
+	{
+
+	}
+}

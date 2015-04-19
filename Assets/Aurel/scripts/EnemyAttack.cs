d520514953cd306431f0e8c6d3a566bc994d76b1
Aurelien Carrere <acarrere@innobec.com>
2015-04-19 13:43:04 -0400

    Enemy Attack !

---
+++ Assets/Aurel/scripts/EnemyAttack.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour 
+{
+	GameObject player;
+	public bool playerInRange;
+	float timer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInRange = true;
+		}
+	}
+	
+	
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInRange = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (playerInRange)
+		{
+			TimerManager timerManager = GameObject.Find ("Managers").GetComponent<TimerManager>();
+			timerManager.isGameOver = true;
+		}
+	}
+}

3e3879114fdf65c7e7cffe6fcfee20bc8d9cd4f2
Bob Quenneville <quenbob@hotmail.com>
2015-04-18 20:29:32 -0400

    Update the tree manager to create an initial forest.

--- Assets/Aurel/scripts/TreesManager.cs
+++ Assets/Aurel/scripts/TreesManager.cs
@@ -15,11 +15,18 @@ public class TreesManager : MonoBehaviour {
 	public float radiusAroundPlayer = 20.0f;
 	public float groundHeight = 10.0f;
 
+	public int initalTreeCount = 0;
+
 	public GameObject player;
 
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating ("Spawn", timeBeforeFirstSpawn, timeBetweenTwoSpawn);
+
+		for(int i = 0; i < initalTreeCount; i++)
+		{
+			AreaSpawn();
+		}
 	}
 	
 	void Spawn () {
@@ -32,16 +39,35 @@ public class TreesManager : MonoBehaviour {
 		float zM = Mathf.Min (zMax, player.transform.position.z + radiusAroundPlayer);
 		float xm = Mathf.Max (xMin, player.transform.position.x - radiusAroundPlayer);
 		float zm = Mathf.Max (zMin, player.transform.position.z - radiusAroundPlayer);
+	
+		SpawnAt(xM, zM, xm, zm);
+	}
+	
+	void AreaSpawn () {
+		if(!canSpawn)
+		{
+			return;
+		}
+		
+		SpawnAt (xMax, zMax, xMin, zMin, true);
+	}
 
+	void SpawnAt(float xM, float zM, float xm, float zm)
+	{
+		SpawnAt (xM, zM, xm, zm, false);
+	}
+
+	void SpawnAt(float xM, float zM, float xm, float zm, bool fullGrown)
+	{
 		Vector3 pos = new Vector3();
 		int nbTry = 0;
 		bool ok = false;
-
+		
 		while(!ok && nbTry < 10) {
 			nbTry++;
 			pos = new Vector3(Random.Range (xm, xM), groundHeight, Random.Range (zm, zM));
 			Collider[] cols = Physics.OverlapSphere(pos, clearRadiusAroundNewSpawn);
-
+			
 			ok = true;
 			foreach (var col in cols)
 			{
@@ -52,11 +78,15 @@ public class TreesManager : MonoBehaviour {
 				}
 			}
 		}
-
+		
 		if (ok)
 		{
 			Quaternion rot = Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), Vector3.up);
-			Instantiate(treeObject, pos, rot);
+
+			if(fullGrown)
+			{
+				GameObject.Instantiate(treeObject, pos, rot);
+			}
 		}
 	}
 }

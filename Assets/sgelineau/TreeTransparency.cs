360de94322192da3bde59840cbe12308b07ed6bb
Aurelien Carrere <acarrere@innobec.com>
2015-04-19 19:36:37 -0400

    new trees

--- Assets/sgelineau/TreeTransparency.cs
+++ Assets/sgelineau/TreeTransparency.cs
@@ -16,10 +16,22 @@ public class TreeTransparency : MonoBehaviour {
 
 	public void setAlpha(float alpha) {
 		if (alpha == 1) {
-			transform.Find("Grown/Tree model/Tree").gameObject.GetComponent<Renderer> ().material.shader = opaqueShader;
+			transform.Find("Grown/Tree model/TreeTrunk").gameObject.GetComponent<Renderer> ().material.shader = opaqueShader;
+			transform.Find("Grown/Tree model/TreeTop1").gameObject.GetComponent<Renderer> ().material.shader = opaqueShader;
+			transform.Find("Grown/Tree model/TreeTop2").gameObject.GetComponent<Renderer> ().material.shader = opaqueShader;
+			transform.Find("Grown/Tree model/TreeTop3").gameObject.GetComponent<Renderer> ().material.shader = opaqueShader;
+			transform.Find("Grown/Tree model/TreeTop4").gameObject.GetComponent<Renderer> ().material.shader = opaqueShader;
 		} else {
-			transform.Find("Grown/Tree model/Tree").gameObject.GetComponent<Renderer> ().material.shader = transparentShader;
-			transform.Find("Grown/Tree model/Tree").gameObject.GetComponent<Renderer> ().material.SetFloat ("_AlphaMultiplier", alpha);
+			transform.Find("Grown/Tree model/TreeTrunk").gameObject.GetComponent<Renderer> ().material.shader = transparentShader;
+			transform.Find("Grown/Tree model/TreeTop1").gameObject.GetComponent<Renderer> ().material.shader = transparentShader;
+			transform.Find("Grown/Tree model/TreeTop2").gameObject.GetComponent<Renderer> ().material.shader = transparentShader;
+			transform.Find("Grown/Tree model/TreeTop3").gameObject.GetComponent<Renderer> ().material.shader = transparentShader;
+			transform.Find("Grown/Tree model/TreeTop4").gameObject.GetComponent<Renderer> ().material.shader = transparentShader;
+			transform.Find("Grown/Tree model/TreeTrunk").gameObject.GetComponent<Renderer> ().material.SetFloat ("_AlphaMultiplier", alpha);
+			transform.Find("Grown/Tree model/TreeTop1").gameObject.GetComponent<Renderer> ().material.SetFloat ("_AlphaMultiplier", alpha);
+			transform.Find("Grown/Tree model/TreeTop2").gameObject.GetComponent<Renderer> ().material.SetFloat ("_AlphaMultiplier", alpha);
+			transform.Find("Grown/Tree model/TreeTop3").gameObject.GetComponent<Renderer> ().material.SetFloat ("_AlphaMultiplier", alpha);
+			transform.Find("Grown/Tree model/TreeTop4").gameObject.GetComponent<Renderer> ().material.SetFloat ("_AlphaMultiplier", alpha);
 		}
 	}
 

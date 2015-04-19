333da92f8efc97f2a361bce835e3d3db41e0fb26
Aurelien Carrere <acarrere@innobec.com>
2015-04-19 17:20:11 -0400

    new sound

---
+++ Assets/sgelineau/woodSound.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class woodSound : MonoBehaviour 
+{
+	private bool alreadyDone = false;
+	private AudioSource fallAudio;
+
+	// Use this for initialization
+	void Start () {
+		fallAudio = GetComponent<AudioSource>();
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(alreadyDone)
+			return;
+
+		if(fallAudio)
+			fallAudio.Play();
+
+		alreadyDone = true;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(alreadyDone)
+			return;
+		
+		if(fallAudio)
+			fallAudio.Play();
+		
+		alreadyDone = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

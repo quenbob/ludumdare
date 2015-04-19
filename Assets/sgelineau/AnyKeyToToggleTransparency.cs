01d13f54428a2808d3113363e924e1552dd5658f
Samuel Gélineau <sgelineau@innobec.com>
2015-04-19 11:01:24 -0400

    preparing to test tree alpha

---
+++ Assets/sgelineau/AnyKeyToToggleTransparency.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnyKeyToToggleTransparency : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject tree = GameObject.Find ("Tree/Model");
+		TreeState treeState = tree.GetComponent<TreeState> ();
+		
+		if (Input.anyKeyDown) {
+		}	
+	}
+}

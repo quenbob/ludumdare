bf6b5dd1331efa02e9edd7f6e905f55d1b00fd07
acarrere <acarrere@innobec.com>
2015-04-21 13:15:35 -0400

    Fix Treefire Particule: Now you can see a lot of trees in fire

--- Assets/Firas/sample1/Assets/Scripts/TreeScript.cs
+++ Assets/Firas/sample1/Assets/Scripts/TreeScript.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 public class TreeScript : MonoBehaviour {
 
 	public bool TreeOnFire = false;
-	public ParticleSystem OnFireParticleSystem;
 	public float TimeToBurnOthers = 7.0f;
 	public int MaxTreesToBurn = 2;
 	public float FireRadius = 15.0f;
@@ -14,13 +13,31 @@ public class TreeScript : MonoBehaviour {
 	private float FireStartedTime = -1.0f;
 	private int numberOfFiresStarted = 0;
 	private bool fireStarted = false;
+	private ParticleSystem OnFireParticleSystem;
+
 	// Use this for initialization
-	void Start () {
-		OnFireParticleSystem.Pause ();
+	void Awake () {
+		initParticule ();
 	}
+
+	private void initParticule()
+	{
+		if (!OnFireParticleSystem) {
+			Transform treefireTr = transform.Find ("TreeFire");
+		
+			if (treefireTr) {
+				GameObject treefire = treefireTr.gameObject;
+			
+				if (treefire) 
+					OnFireParticleSystem = treefire.GetComponent<ParticleSystem> ();
+			}
+		}
+	}
+
 
 	public void SetThisOnFire()
 	{
+		initParticule ();
 		TreeOnFire = true;
 	}
 	
@@ -31,8 +48,9 @@ public class TreeScript : MonoBehaviour {
 		{
 			if(!fireStarted)
 			{
-				OnFireParticleSystem.Play ();
 				fireStarted = true;
+				if (OnFireParticleSystem)
+					OnFireParticleSystem.Play ();
 			}
 
 			if(FireStartedTime<0)
@@ -40,95 +58,39 @@ public class TreeScript : MonoBehaviour {
 				FireStartedTime = Time.time;
 
 			}
-			else if((Time.time - FireStartedTime)>BurnLifetime){
+			else if((Time.time - FireStartedTime)>BurnLifetime) {
 				GetComponentInChildren<TreeFalling>().cutDownFrom(new Vector3(1.0f,0.0f,0.0f),false);
-				Vector3 direction = Vector3.Normalize(transform.position);
-				//GetComponent<TreeFalling>().cutDownFrom(new Vector3(0.0f,0.0f,0.0f),false);
 			}
 
+			if(numberOfFiresStarted < MaxTreesToBurn)
 			{
-
-				if(numberOfFiresStarted < MaxTreesToBurn)
+				if((Time.time - FireStartedTime) > (TimeToBurnOthers * (numberOfFiresStarted+1)))
 				{
-					if((Time.time - FireStartedTime) > (TimeToBurnOthers * (numberOfFiresStarted+1)))
+					//Burn nearest tree
+					Collider[] cols = Physics.OverlapSphere(transform.position, FireRadius);
+
+					foreach (var col in cols)
 					{
-						//Burn nearest tree
-
-						//float shortestDistance = 999999;
-						//int shortestIndex = -1;
-						List<GameObject> allTrees = new List<GameObject>();// = GameObject.FindObjectsOfType(typeof(Tree)) as Tree[];
-
-						Collider[] cols = Physics.OverlapSphere(transform.position, FireRadius);
-						
-
-						foreach (var col in cols)
+						if (col.attachedRigidbody && col.attachedRigidbody.gameObject.tag == "Tree")
 						{
-							if (col.attachedRigidbody && col.attachedRigidbody.gameObject.tag == "Tree")
+							GameObject treeObject = col.attachedRigidbody.gameObject.transform.parent.gameObject;
+
+							if(treeObject != gameObject && treeObject.GetComponentInChildren<TreeScript>().TreeOnFire==false)
 							{
-
-								//Debug.Log(col.name);
-								//allTrees.Add(col.attachedRigidbody.gameObject);
-								//col.attachedRigidbody.gameObject.GetComponent<TreeScript>().SetThisOnFire();
-								GameObject treeObject = col.attachedRigidbody.gameObject.transform.parent.gameObject;
-
-								if(treeObject != gameObject && treeObject.GetComponentInChildren<TreeScript>().TreeOnFire==false)
+								treeObject.GetComponentInChildren<TreeScript>().SetThisOnFire();
+								
+								numberOfFiresStarted = numberOfFiresStarted + 1;
+								
+								if(MaxTreesToBurn<numberOfFiresStarted)
 								{
-									//Debug.Log(subModel.name);
-									//GameObject model = subModel.transform.parent.gameObject;
-									//GameObject treeObject = model.transform.parent.gameObject;
-									treeObject.GetComponentInChildren<TreeScript>().SetThisOnFire();
-									
-									numberOfFiresStarted = numberOfFiresStarted + 1;
-									
-									if(MaxTreesToBurn<numberOfFiresStarted)
-									{
-										break;
-									}
-									
+									break;
 								}
-
-
-							
-
 							}
 						}
-
-
-
-						/*for (int i=0;i<allTrees.Length;i++) {
-
-							Transform treeTrans = allTrees[i].transform;
-							float dist = Vector3.Distance (treeTrans.position,transform.position);
-							if(dist < shortestDistance)
-							{
-								dist= shortestDistance;
-								shortestIndex = i;
-							}
-						}*/
-
-						/*if(shortestIndex>-1)
-						{
-							numberOfFiresStarted = numberOfFiresStarted + 1;
-							allTrees[shortestIndex].GetComponent<TreeScript>().SetThisOnFire();
-						}
-
-*/
-
-
-
 					}
 				}
-
 			}
-
-
-
 		}
-		if(!TreeOnFire)
-		{
-			OnFireParticleSystem.Stop ();
-		}
-	
 	}
 }
 
